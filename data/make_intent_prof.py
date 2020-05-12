ff8aaaf8c4234d9a7be4fa0a7f51a35f7eb6cdf7
Jungyoon Han <31722820+JungyoonHan@users.noreply.github.com>
2020-05-12 11:55:32 +0900

    수정-200512

--- data/make_intent_prof.py
+++ data/make_intent_prof.py
@@ -4,36 +4,30 @@ import numpy as np
 prof = pd.read_csv('professor.csv', header=None)
 prof = list(prof[0])
 
-inform = ['이메일', '전화번호', '연락처', '연구실']
+inform = ['','정보'] # For making 정보.csv
+mail = ['메일', '이메일'] # For making 이메일.csv
+phone = ['전화번호', '연락처'] # For making 연락처.csv
+room = ['연구실'] # For making 연구실.csv
 please = ['', '알려줘', '알려줄래', '찾아줘', '찾아줄래', '알아?']
 
-intent = [] # 질문 set
-tent = [] # 의도
+question = [] # 질문 set
+intent = [] # 의도
 for i in range(len(prof)):
-    for j in range(len(inform)):
+    for j in range(len(inform)): # 만들 때 바꾸기 -> inform / mail / phone / room
         for k in range(len(please)):
-            sentence = prof[i]+ ' 교수님 '+ inform[j] + ' ' + please[k]
-            intent.append(sentence)
-            if j == 0: # 이메일
-                tent.append('교수이메일')
-            elif j == 1 or j == 2: # 전화번호, 연락처
-                tent.append('교수연락처')
-            else: # 연구실
-                tent.append('교수연구실')
+            sentence = prof[i]+ ' 교수님 '+ inform[j] + ' ' + please[k] # 만들 때 바꾸기 -> inform / mail / phone / room
+            intent.append('정보') # 만들 때 바꾸기 -> 정보 / 이메일 / 연락처 / 연구실
+
+# 연구실.csv 만들 때만 사용
 for name in prof:
     sentence = name + ' 교수님 연구실 어디야?'
-    intent.append(sentence)
-    tent.append('교수연구실')
-for name in prof:
-    for i in range(3):
-        sentence = name + ' 교수님 정보 ' + please[i]
-        intent.append(sentence)
-        tent.append('교수님정보') #?
+    question.append(sentence)
+    tent.append('연구실')
 
-intent.extend(tent) # len(intent) = 45684
-arr_intent = np.array(intent)
-arr_intent = arr_intent.reshape(-1, len(tent)) # len(tent) = 22932
+question.extend(tent)
+arr_intent = np.array(question)
+arr_intent = arr_intent.reshape(-1, len(intent))
 arr_intent = arr_intent.transpose()
 
 new_intent = pd.DataFrame(arr_intent)
-new_intent.to_csv('train_intent.csv')
+new_intent.to_csv('정보.csv') # 만들 때 바꾸기 -> 정보 / 이메일 / 연락처 / 연구실
